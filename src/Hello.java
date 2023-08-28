1774d083ef4cb291dd69e0235794328698049270
[Ali Keshk] <[akeshk2@huskers.unl.edu]>
2023-08-28 15:56:56 -0500

    added name and major

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
   public static void main(String args[]) {
     System.out.println("Jacob Sykora!");
     System.out.println("Computer Engineering!");
+    System.out.println("Ali Keshk!");
+    System.out.println("Computer Science!");
   }
 
 }
